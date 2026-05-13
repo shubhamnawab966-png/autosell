2984685b4846ea8c29157eb5d9d34a5ae703da58
Shubham <shubhamnawab966@gmail.com>
2026-05-13 19:09:01 +0700

    fix CORS for Vercel

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://autosell-vert.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
