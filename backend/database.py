1231f3e4668809e2bdde1012dd8711c291870e05
Shubham <shubhamnawab966@gmail.com>
2026-05-14 13:32:50 +0700

    add PostgreSQL support

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
 
-DATABASE_URL = "sqlite:///./autosell.db"
+import os
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./autosell.db")
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
